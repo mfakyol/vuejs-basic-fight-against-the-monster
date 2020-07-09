e3f302919692c27b0d53b5cc9b897e897ad94eb2
mfakyol <55063801+mfakyol@users.noreply.github.com>
2020-07-09 22:24:30 +0300

    variables-added-and-template-improved

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const vm = new Vuew({
     el:"#app",
     data:{
-
+        isStarted:false,
+        playerHealth: 100,
+        monsterHealth: 100,
+        playerBaseDamage: 12,
+        monsterBaseDamage: 10,
+        playerBaseHeal: 15,
+        monsterBaseHeal: 14,
+        playerFocus:25,
+        monsterFocus,
+        log: []
     },
     methods: {
         
